fce51f9694f094787a4c09825640911df4b094c0
noraneko <masima206@gmail.com>
2024-11-29 23:42:42 +0900

    Deleted WorkerPosition Class, simplyfied GenerateNumber() method

--- InspectorLib/FunctionInsp.cs
+++ InspectorLib/FunctionInsp.cs
@@ -10,20 +10,13 @@ namespace InspectorLib
     public class FunctionInsp
     {
         private List<Worker> workers = new List<Worker>();
-        private List<WorkerPosition> positions = new List<WorkerPosition>();
         private Worker mainInspector;
         private string carInspectionName;
 
         // Инициализация начальных данных через конструктор класса
         public FunctionInsp()
         {
-            // Создаём должность с названием "Инспектор", окладом 75 тысяч и ID=2
-            AddPosition("Инспектор", 75000.0, 2);
-
-            AddPosition("Главный инспектор", 120000.0, 1);
-
             carInspectionName = "Автоинспекция г. Чита";
-
 
             // Добавляем начальные данные
             // При добавлении сотрудников чепез AddWorker(fullname), возраст = 0, должность выбирается первой из списка должностей positions
@@ -32,7 +25,7 @@ namespace InspectorLib
             AddWorker("Миронов А. В.");
 
             // Добавляем Главного инспектора через перегруженный метод, принимающий отдельно ФИО, возраст и ID должности
-            AddWorker("Василий", "Васильев", "Иванович", 45, 1);
+            AddWorker("Василий", "Васильев", "Иванович");
 
             // Устанавливаем главного инспетора в первый раз
             SetInspector("Васильев Василий Иванович");
@@ -65,7 +58,8 @@ namespace InspectorLib
         // Устанавливает нового главного инспектора
         public void SetInspector(string fullname)
         {
-            if (fullname == null) {
+            if (fullname == null)
+            {
                 Console.WriteLine("Ошибка! Имя нового инспекотра не содержит значения.");
                 return;
             }
@@ -75,6 +69,7 @@ namespace InspectorLib
                 if (worker.getFullName() == fullname)
                 {
                     mainInspector = worker;
+                    Console.WriteLine("Установлен новый главный инспектор!");
                     return;
                 }
             }
@@ -89,7 +84,7 @@ namespace InspectorLib
         {
             var rand = new Random();
 
-            string[] symbols = { "а", "в", "е", "к", "м", "н", "о", "р", "с", "т", "у", "х"};
+            string[] symbols = { "а", "в", "е", "к", "м", "н", "о", "р", "с", "т", "у", "х" };
 
             return GenerateNumber(rand.Next(1, 9999), symbols[rand.Next(0, symbols.Length)], code);
         }
@@ -126,40 +121,18 @@ namespace InspectorLib
 
             result = symbol.ToUpper();
 
-            if (number < 10)
-            {
-                result = result + "000" + number.ToString();
-            }
-            else if (number < 100)
-            {
-                result = result + "00" + number.ToString();
-            }
-            else if (number < 1000)
-            {
-                result = result + "0" + number.ToString();
-            }
-            else
-            {
-                result += number.ToString();
-            }
+            result += number.ToString("0000");
 
-            if (code < 10)
-            {
-                result += "_0" + code.ToString();
-            }
-            else
-            {
-                result += "_" + code.ToString();
-            }
-
-            
+            result += code.ToString("_00");
 
             return result;
         }
 
+
+
         // Возвращает массив, состоящий из ФИО работников
         public string[] GetWorker()
-        {            
+        {
             string[] result = new string[workers.Count];
             for (int i = 0; i < workers.Count; i++)
             {
@@ -171,32 +144,9 @@ namespace InspectorLib
 
 
         // Добавляет работника с указанными данными
-        public void AddWorker(string firtsName, string lastName, string patronymic, int age, int positionID)
+        public void AddWorker(string firtsName, string lastName, string patronymic)
         {
-            if (positionID < 0)
-            {
-                Console.WriteLine("ID должности не может быть отрицательным!");
-                return;
-            }
-
-            // для проверки, есть ли должность с заданным ID в списке
-            WorkerPosition tempPos = new WorkerPosition(-1, "", 0);
-            foreach (WorkerPosition pos in positions)
-            {
-                if (pos.getId() == positionID)
-                {
-                    tempPos = pos;
-                    break;
-                }
-            }
-
-            if (tempPos.id == -1)
-            {
-                Console.WriteLine("Не найдена должность с таким ID!");
-                return;
-            }
-
-            workers.Add(new Worker(workers.Count, firtsName, lastName, patronymic, age, tempPos));
+            workers.Add(new Worker(workers.Count, firtsName, lastName, patronymic));
             Console.WriteLine($"Сотрудник {lastName} {firtsName} {patronymic} успешно добавлен!");
         }
 
@@ -205,110 +155,47 @@ namespace InspectorLib
         // Добавляет работника "заглушку"
         public void AddWorker()
         {
-            if (positions.Count == 0)
-            {
-                Console.WriteLine("Невозможно добавить сотрудника! Нету должностей.");
-                return;
-            }
-            workers.Add(new Worker(workers.Count, "Имя", "Фамилия", "Отчество", 0, positions[0]));
+            workers.Add(new Worker(workers.Count, "Имя", "Фамилия", "Отчество"));
             Console.WriteLine($"Сотрудник {workers.Last().getFullName()} успешно добавлен!");
         }
+
 
 
         // Добавляет сотрудника, принимая на вход одну строку
         public void AddWorker(string fullname)
         {
-            if (positions.Count == 0)
-            {
-                Console.WriteLine("Невозможно добавить сотрудника! Нету должностей.");
-                return;
-            }
             string[] nameParts = fullname.Split(' ');
-            if (nameParts.Length != 3) {
+            if (nameParts.Length != 3)
+            {
                 Console.WriteLine("Не удалось прочитать имя");
                 return;
             }
-            workers.Add(new Worker(workers.Count, nameParts[1], nameParts[0], nameParts[2], 0, positions[0]));
+            workers.Add(new Worker(workers.Count, nameParts[1], nameParts[0], nameParts[2]));
             Console.WriteLine($"Сотрудник {fullname} успешно добавлен!");
         }
 
 
-        // Добавляет новую должность
-        public void AddPosition(string name, double salary, int id)
+
+        // Класс работника
+        public class Worker
         {
-            if (salary < 0 || id < 0)
+            int id;
+            string firstName;
+            string lastName;
+            string patronymic;
+
+            public Worker(int id, string firstName, string lastName, string patronymic)
             {
-                Console.WriteLine("Значения не могут быть отрицательными!");
-                return;
+                this.id = id;
+                this.firstName = firstName;
+                this.lastName = lastName;
+                this.patronymic = patronymic;
             }
-            foreach (WorkerPosition pos in positions)
+
+            public string getFullName()
             {
-                if (pos.id == id)
-                {
-                    Console.WriteLine("Должность с таким ID уже существует!");
-                    return;
-                }
+                return $"{lastName} {firstName} {patronymic}";
             }
-
-            positions.Add(new WorkerPosition(id, name, salary));
-        }
-    }
-
-
-
-    // Класс должности
-    public class WorkerPosition
-    {
-        public int id;
-        public string name;
-        public double salary;
-
-        public WorkerPosition(int id, string name, double salary)
-        {
-            this.id = id;
-            this.name = name;
-            this.salary = salary;
-        }
-        public int getId() { return id; }
-    }
-
-
-
-    // Класс работника
-    public class Worker
-    {
-        int id;
-        int age;
-        string firstName;
-        string lastName;
-        string patronymic;
-
-        WorkerPosition position;
-
-        public Worker(int id, string firstName, string lastName, string patronymic, int age, WorkerPosition position)
-        {
-            this.id = id;
-            this.age = age;
-            this.firstName = firstName;
-            this.lastName = lastName;
-            this.patronymic = patronymic;
-            this.position = position;
-        }
-
-
-        public void SetAge(int newAge) // Пример публичного метода "сеттера"
-        {                            // Лучше использовать приватные переменные, доступ к которым производится с помощью специальных публичных методов.
-            if (newAge < 0)           // Так, например, можно добавить проверку на корректность вводимых данных.
-            {
-                throw new ArgumentOutOfRangeException("Возраст не может быть меньше нуля!");
-            }
-            age = newAge;
-        }
-
-
-        public string getFullName()
-        {
-            return $"{lastName} {firstName} {patronymic}";
         }
     }
 }
